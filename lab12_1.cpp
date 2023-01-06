9015e1a97e514434fa49b77b7005a907bd781521
Entity014 <radlove.012@gmail.com>
2023-01-06 15:43:25 +0700

    lab12_1.cpp completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,22 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    string x;
+    string a[9] = {"A" ,"B+" ,"B" ,"C+" ,"C" ,"D+" ,"D" ,"F" ,"W"};
+    int i = 0;
+    int k;
+    cout << "Press Enter 3 times to reveal your future.";
+    while(true)
+    {
+        getline(cin, x);
+        if(x == "") i++;
+        if(i >= 3) break;
+    }
+    srand(time(0));
+    k = rand()%(sizeof(a)/sizeof(a[0]));
+    cout << "You will get " << a[k] << " in this 261102.";
+}
